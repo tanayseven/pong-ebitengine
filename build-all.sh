5835cfec7d3018627274ab88bb87377af80b3f1a
Tanay PrabhuDesai <tanayseven@gmail.com>
2024-03-05 23:24:24 +0530

    Add build all scripts with OS specific code

--- build-all.sh
+++ build-all.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
 
-# Build for Windows
-env GOOS=windows GOARCH=amd64 go build -o build/pong.exe .
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
-# Build for Linux
-env GOOS=linux GOARCH=amd64 go build -o build/pong .
+  echo "Building for Windows"
+  env GOOS=windows GOARCH=amd64 go build -o build/pong.exe .
 
-# Build for Web
-cp $(go env GOROOT)/misc/wasm/wasm_exec.js public/
-env GOOS=js GOARCH=wasm go build -o public/pong.wasm
-zip build/pong-web-go.zip public/*
+  echo "Building for Linux"
+  env GOOS=linux GOARCH=amd64 go build -o build/pong .
+
+  echo "Building for Web"
+  cp $(go env $GOROOT)/misc/wasm/wasm_exec.js public/
+  env GOOS=js GOARCH=wasm go build -o public/pong.wasm
+  zip build/pong-web-go.zip public/*
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+
+  echo "Building for Mac Intel"
+  env GOOS=darwin GOARCH=amd64 go build -o build/pong-intel .
+
+  echo "Building for Mac Apple Silicon"
+  env GOOS=darwin GOARCH=arm64 go build -o build/pong-apple-silicon .
+
+else
+  echo "Unsupported OS"
+fi
