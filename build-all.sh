1becb8b7e4962d57b71a4a3edc3be3241346d646
Tanay PrabhuDesai <tanayseven@gmail.com>
2024-03-05 23:10:55 +0530

    Add web assembly build

--- build-all.sh
+++ build-all.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
+
+# Build for Windows
 env GOOS=windows GOARCH=amd64 go build -o build/pong.exe .
+
+# Build for Linux
 env GOOS=linux GOARCH=amd64 go build -o build/pong .
+
+# Build for Web
+cp $(go env GOROOT)/misc/wasm/wasm_exec.js public/
+env GOOS=js GOARCH=wasm go build -o public/pong.wasm
+zip build/pong-web-go.zip public/*
