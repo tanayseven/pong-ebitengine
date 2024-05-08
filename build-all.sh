fb1147f78aa25ab9644613b3053c122dee8d900a
Tanay PrabhuDesai <tanayseven@gmail.com>
2024-05-08 18:15:05 +0530

    Fix building of wasm

--- build-all.sh
+++ build-all.sh
@@ -9,7 +9,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   env GOOS=linux GOARCH=amd64 go build -o build/pong-linux-amd64 .
 
   echo "Building for Web"
-  cp $(go env $GOROOT)/misc/wasm/wasm_exec.js public/
+  cp "$GOROOT"/misc/wasm/wasm_exec.js public/
   env GOOS=js GOARCH=wasm go build -o public/pong.wasm
   zip build/pong-web-go.zip public/*
 
