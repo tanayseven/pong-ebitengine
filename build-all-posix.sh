4e1b2bd488b8d2308e6446417fc057dddb975f49
Tanay PrabhuDesai <tanayseven@gmail.com>
2024-05-11 16:47:04 +0530

    Fix the build process

--- build-all-posix.sh
+++ build-all-posix.sh
@@ -9,7 +9,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   env GOOS=linux GOARCH=amd64 go build -o build/pong-linux .
 
   echo "Building for Web"
-  cp "$GOROOT"/misc/wasm/wasm_exec.js public/
+  cp $(go env GOROOT)/misc/wasm/wasm_exec.js ./public/
   env GOOS=js GOARCH=wasm go build -o public/pong.wasm
   zip build/pong-web-wasm.zip public/*
 
@@ -20,6 +20,11 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 
   echo "Building for Mac Apple Silicon"
   env GOOS=darwin CGO_ENABLED=1 GOARCH=arm64 go build -o build/pong-macos-apple-silicon .
+
+  echo "Building for Web"
+  cp $(go env GOROOT)/misc/wasm/wasm_exec.js ./public/
+  env GOOS=js GOARCH=wasm go build -o public/pong.wasm
+  zip build/pong-web-go.zip public/*
 
 else
 
